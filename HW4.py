64aff071fcc594bd8c5da3508d0d4cd57a2fbd7e
sujikim <sujikim@umich.edu>
2016-11-30 00:58:23 -0500

    changed display color

--- HW4.py
+++ HW4.py
@@ -2,31 +2,31 @@ import pygame
 pygame.init()
 
 
-#create colors
+# creating colors
 white = (255,255,255)
 black = (0,0,0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
-#position vars
+# assigning position variables
 x_pos = 0
 y_pos = 0
 x_delta = 0
 y_delta = 0
 clock = pygame.time.Clock()
 
-#create a surface
+# create a game display surface
 gameDisplay = pygame.display.set_mode((800,600)) #initialize with a tuple
 
-#lets add a title, aka "caption"
-pygame.display.set_caption("Drawing Shapes!")
+# lets add a title, aka "caption"
+pygame.display.set_caption("Let's find Colleen!")
 pygame.display.update() #only updates portion specified
 
 
 gameExit = False
 while not gameExit:
-	gameDisplay.fill(white)
+	gameDisplay.fill(black)
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -52,6 +52,5 @@ while not gameExit:
 
 
 
-#required
 pygame.quit()
-quit()				#exits python
+quit() # exits python
